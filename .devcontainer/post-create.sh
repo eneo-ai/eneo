c65b8505feba8051061f51bea71f61ec9ca78445
Alexander Andersson <alexander@devil.se>
2025-08-20 07:45:24 +0000

    add searxng to devcontainer setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -16,10 +16,9 @@ cd /workspace/backend
 python -m poetry config virtualenvs.in-project true
 python -m poetry install
 
-# Install pre-commit globally and setup hooks
+# Install pre-commit globally (hooks will be installed in post-start)
 cd /workspace
 python -m pip install pre-commit
-pre-commit install
 
 # Install Node.js dependencies
 cd /workspace/frontend
