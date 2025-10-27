78b1f5204ae4eec0dcc1d196a8ec6fe9e0e36896
Andreas Larsson <thelurenator@gmail.com>
2025-10-27 15:28:31 +0100

    New feature: run.sh reads new env-variable START_ROLE so the correct command is used. This will support several different start "modes".

--- backend/run.sh
+++ backend/run.sh
@@ -2,18 +2,47 @@
 
 set -e
 
-# Skip Alembic migrations in OpenAPI-only mode
-if [[ "${OPENAPI_ONLY_MODE,,}" != "true" ]]; then
+# Default start
+function start_backend_default() {
+  # Skip Alembic migrations in OpenAPI-only mode
+  if [[ "${OPENAPI_ONLY_MODE,,}" != "true" ]]; then
     alembic upgrade head
-fi
+  fi
 
-if [[ -z "${NUM_WORKERS}" ]]; then
+  if [[ -z "${NUM_WORKERS}" ]]; then
     workers=3
-else
+  else
     workers=$NUM_WORKERS
-fi
+  fi
 
-echo "Starting intric.ai with $workers workers"
-echo "Launching... Go, intric.ai!"
+  echo "Starting intric.ai with $workers workers"
+  echo "Launching... Go, intric.ai!"
 
-exec gunicorn src.intric.server.main:app --workers $workers --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+  exec gunicorn src.intric.server.main:app --workers $workers --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+}
+
+# new start
+function start_backend_arq() {
+  arq src.intric.worker.arq.WorkerSettings
+}
+
+# worker start
+function start_worker_default() {
+  poetry run arq intric.worker.arq.WorkerSettings
+}
+
+
+case "${START_ROLE}" in
+    ""|"DEFAULT")
+        start_backend_default
+        ;;
+    "BACKEND_ARQ")
+        start_backend_arq
+        ;;
+    "WORKER_DEFAULT")
+        start_worker_default
+        ;;
+esac
+
+
+
