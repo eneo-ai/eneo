842f00263aabdb5e202b67c89f3ac1421896bca7
CCimen <cagricode@gmail.com>
2025-11-03 10:11:49 +0000

    Fixed the integration tests and also merged the alembic revisions into a single one.

---
+++ backend/alembic/versions/c24c8d895bcd_merge_tenant_scoped_templates_and_embed_.py
@@ -0,0 +1,22 @@
+"""merge 
+  tenant_scoped_templates and embed_max_batch_size
+Revision ID: c24c8d895bcd
+Revises: tenant_scoped_templates, embed_max_batch_size
+Create Date: 2025-11-03 09:54:38.852701
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic
+revision = 'c24c8d895bcd'
+down_revision = ('tenant_scoped_templates', 'embed_max_batch_size')
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
