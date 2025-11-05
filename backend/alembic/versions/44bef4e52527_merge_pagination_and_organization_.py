c254ec68b1d87d1d2814f65d5c796aed4f23ce2e
CCimen <cagricode@gmail.com>
2025-11-05 14:22:42 +0100

    chore: add Alembic merge migration for pagination and organization features

---
+++ backend/alembic/versions/44bef4e52527_merge_pagination_and_organization_.py
@@ -0,0 +1,19 @@
+"""merge pagination and organization migrations
+Revision ID: 44bef4e52527
+Revises: 1ca224418b7b, 8b997505ac27
+Create Date: 2025-11-05 13:11:37.177558
+"""
+
+
+
+# revision identifiers, used by Alembic
+revision = '44bef4e52527'
+down_revision = ('1ca224418b7b', '8b997505ac27')
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
