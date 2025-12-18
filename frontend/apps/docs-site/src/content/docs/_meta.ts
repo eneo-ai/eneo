0b2042585a341a93fccee019138462446040292a
Alexander Andersson <alexander@devil.se>
2025-12-18 09:23:14 +0100

    Feature/docs site (#54)
    * initial commit apps/docs-site
    
    * theme fixes + temp about page
    
    * build docs-site image
    
    * skip steps not related to docs
    
    * build docs-site image on develop and main push
    
    * WIP docs update
    
    * update content
    
    * migrate pnpm to bun
    
    * separate docs gh workflow
    
    * update bun.lock
    
    * copy all workspace files to be able to build docs-site
    
    * push the image on prs
    
    * add pagefind to enable search

---
+++ frontend/apps/docs-site/src/content/docs/_meta.ts
@@ -0,0 +1,18 @@
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  'getting-started': {
+    title: 'Getting Started',
+    type: 'page',
+  },
+  architecture: {
+    title: 'Architecture',
+    type: 'page',
+  },
+  api: {
+    title: 'API Reference',
+    type: 'page',
+  },
+}
+
+export default meta;
