47b2383bad52ac7f24009b0212050726e204ba14
Alexander Andersson <alexander@oddlyeven.se>
2026-01-09 10:09:51 +0100

    use markdown for images to resolve basePath correctly

--- frontend/apps/docs-site/src/mdx-components.js
+++ frontend/apps/docs-site/src/mdx-components.js
@@ -1,12 +1,27 @@
-import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
 
-// Get the default MDX components
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+
+// Get the default MDX components from nextra-theme-docs
 const themeComponents = getThemeComponents()
 
-// Merge components
+// Custom img component that prepends basePath to absolute paths
+function Img({ src, alt, ...props }) {
+  const resolvedSrc = src?.startsWith('/') ? `${basePath}${src}` : src
+  return (
+    <img
+      src={resolvedSrc}
+      alt={alt}
+      className="w-full max-w-5xl mx-auto"
+      {...props}
+    />
+  )
+}
+
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
+    img: Img,
     ...components
   }
 }
