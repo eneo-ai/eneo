2047b1ae952a0ba8e2faaaf04e8ef9e2900f8eb7
Max Eriksson <max.eriksson@devize.se>
2026-03-18 22:18:42 +0100

    Fix: shorten alembic revision ID to fit varchar(32)

--- backend/alembic/versions/20260318_add_icon_id_to_group_chats.py
+++ backend/alembic/versions/20260318_add_icon_id_to_group_chats.py
@@ -1,6 +1,6 @@
 """add icon_id to group_chats
 
-Revision ID: 20260318_add_icon_id_to_group_chats
+Revision ID: gc_icon_id
 Revises: mcp_security_classification
 Create Date: 2026-03-18
 """
@@ -9,7 +9,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "20260318_add_icon_id_to_group_chats"
+revision = "gc_icon_id"
 down_revision = "mcp_security_classification"
 branch_labels = None
 depends_on = None
